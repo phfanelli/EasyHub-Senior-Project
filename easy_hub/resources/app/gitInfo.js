1c2aea3df8d6e40c2e851d90bf3b66923235457f
desidia26 <desidiawvu@gmail.com>
2017-04-18 22:11:40 -0400

    few gitInfo changes

--- easy_hub/resources/app/gitInfo.js
+++ easy_hub/resources/app/gitInfo.js
@@ -1,14 +1,14 @@
 var gitInfo = gitInfo || {};
 
-gitInfo.commitMessage="Records the changes made to the repository. Allows the user to provide a" +
-    " message to describe the changes that were made." +
+gitInfo.commitMessage="Records the changes made on the current branch to the repository. Allows the user to provide a" +
+    " message to describe the changes that were made and automatically adds all new files." +
 "\n\n\nThis must be done before PUSH can be used"    ;
 
-gitInfo.pushMessage="Updates the current repository with the changes included in the last commit." +
-    " Requires a commit to occur first.";
+gitInfo.pushMessage="Updates the current remote branch with the changes included in the current outstanding commits" +
+    " Requires a commit to have occured first.";
 
-gitInfo.pullMessage="Brings down changes from currently selected repository and integrates them " +
-    "with your current branch.";
+gitInfo.pullMessage="Brings down changes from currently selected remote branch and integrates them " +
+    "with your current local branch";
 
 gitInfo.cloneMessage="Brings down a copy of the selected repository and stores it in the specified" +
     " folder.";
